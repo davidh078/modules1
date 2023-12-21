cca3c359c94859ab714b72ad6cc925d14544bd3e
davidh078 <aruninr03@gmail.com>
2023-12-21 11:10:28 +0530

    my new codes

--- src/com/ppo/haierAC.java
+++ src/com/ppo/haierAC.java
@@ -1,5 +1,8 @@
 package com.ppo;
 
 public class haierAC {
-
+public static void main(String[] args) {
+	System.out.println("this is a Ac company");
+	System.out.println("this is a arunkumar branch");
+}
 }
